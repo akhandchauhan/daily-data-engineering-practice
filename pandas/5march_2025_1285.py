2073fd6be91a2b56c7a5ab73cd8c83cb87d037c0
akhandchauhan <coderasdhero@gmail.com>
2025-12-20 11:13:20 +0530

    learned a new approach for finding continuous ranges

---
+++ pandas/5march_2025_1285.py
@@ -0,0 +1,111 @@
+# -- 1285. Find the Start and End Number of Continuous Ranges
+# -- Description
+# -- Table: Logs
+# -- +---------------+---------+
+# -- | Column Name   | Type    |
+# -- +---------------+---------+
+# -- | log_id        | int     |
+# -- +---------------+---------+
+# -- log_id is the column of unique values for this table.
+# -- Each row of this table contains the ID in a log Table.
+# -- Write a solution to find the start and end number of continuous ranges in the table Logs.
+# -- Return the result table ordered by start_id.
+# -- The result format is in the following example.
+# -- Example 1:
+# -- Input: 
+# -- Logs table:
+# -- +------------+
+# -- | log_id     |
+# -- +------------+
+# -- | 1          |
+# -- | 2          |
+# -- | 3          |
+# -- | 7          |
+# -- | 8          |
+# -- | 10         |
+# -- +------------+
+# -- Output: 
+# -- +------------+--------------+
+# -- | start_id   | end_id       |
+# -- +------------+--------------+
+# -- | 1          | 3            |
+# -- | 7          | 8            |
+# -- | 10         | 10           |
+# -- +------------+--------------+
+
+import pandas as pd
+
+logs = {
+    'log_id' : [1,2,3,7,8,10]
+    }
+df = pd.DataFrame(logs)
+
+df['diff'] = df['log_id'] - df['log_id'].rank(ascending=True)
+
+# Step 2: Group by the identifier and compute start and end of each range
+df = df.groupby('diff')['log_id'].agg(['min', 'max']).reset_index()
+df = df.rename(columns={'min': 'start_id', 'max': 'end_id'})
+
+# Step 4: Sort by 'start_id' and select the required columns
+df = df.sort_values(by='start_id')[['start_id', 'end_id']]
+
+print(df)
+
+
+
+# -- 1270. All People Report to the Given Manager
+# -- Description
+# -- Table: Employees
+# -- +---------------+---------+
+# -- | Column Name   | Type    |
+# -- +---------------+---------+
+# -- | employee_id   | int     |
+# -- | employee_name | varchar |
+# -- | manager_id    | int     |
+# -- +---------------+---------+
+# -- employee_id is the column of unique values for this table.
+# -- Each row of this table indicates that the employee with ID employee_id and name employee_name 
+# --reports his work to his/her direct manager with manager_id
+# -- The head of the company is the employee with employee_id = 1.
+# -- Write a solution to find employee_id of all employees that directly or indirectly report their 
+# --work to the head of the company.
+# -- The indirect relation between managers will not exceed three managers as the company is small.
+# -- Return the result table in any order.
+# -- The result format is in the following example.
+# -- Example 1:
+
+# -- Input: 
+# -- Employees table:
+# -- +-------------+---------------+------------+
+# -- | employee_id | employee_name | manager_id |
+# -- +-------------+---------------+------------+
+# -- | 1           | Boss          | 1          |
+# -- | 3           | Alice         | 3          |
+# -- | 2           | Bob           | 1          |
+# -- | 4           | Daniel        | 2          |
+# -- | 7           | Luis          | 4          |
+# -- | 8           | Jhon          | 3          |
+# -- | 9           | Angela        | 8          |
+# -- | 77          | Robert        | 1          |
+# -- +-------------+---------------+------------+
+# -- Output: 
+# -- +-------------+
+# -- | employee_id |
+# -- +-------------+
+# -- | 2           |
+# -- | 77          |
+# -- | 4           |
+# -- | 7           |
+# -- +-------------+
+
+import pandas as pd
+data = {
+    "employee_id": [1, 3, 2, 4, 7, 8, 9, 77],
+    "employee_name": ["Boss", "Alice", "Bob", "Daniel", "Luis", "Jhon", "Angela", "Robert"],
+    "manager_id": [1, 3, 1, 2, 4, 3, 8, 1]
+}
+
+df1 = pd.DataFrame(data)
+df1 = df1.merge(df1,left_on='manager_id',right_on ='employee_id', how='left').merge(df1,left_on='manager_id_y',right_on ='employee_id', how='left')
+df1 = df1.query("employee_name_x != 'Boss' and manager_id == 1")[["employee_id_x"]]
+print(df1)
---
+++ sql/25sept_2024_1285_1308.sql
@@ -0,0 +1,188 @@
+-- 1308. Running Total for Different Genders
+-- Description
+-- Table: Scores
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | player_name   | varchar |
+-- | gender        | varchar |
+-- | day           | date    |
+-- | score_points  | int     |
+-- +---------------+---------+
+-- (gender, day) is the primary key (combination of columns with unique values) for this table.
+-- A competition is held between the female team and the male team.
+-- Each row of this table indicates that a player_name and with gender has scored score_point in someday.
+-- Gender is 'F' if the player is in the female team and 'M' if the player is in the male team.
+-- Write a solution to find the total score for each gender on each day.
+-- Return the result table ordered by gender and day in ascending order.
+-- The result format is in the following example.
+-- Example 1:
+-- Input: 
+-- Scores table:
+-- +-------------+--------+------------+--------------+
+-- | player_name | gender | day        | score_points |
+-- +-------------+--------+------------+--------------+
+-- | Aron        | F      | 2020-01-01 | 17           |
+-- | Alice       | F      | 2020-01-07 | 23           |
+-- | Bajrang     | M      | 2020-01-07 | 7            |
+-- | Khali       | M      | 2019-12-25 | 11           |
+-- | Slaman      | M      | 2019-12-30 | 13           |
+-- | Joe         | M      | 2019-12-31 | 3            |
+-- | Jose        | M      | 2019-12-18 | 2            |
+-- | Priya       | F      | 2019-12-31 | 23           |
+-- | Priyanka    | F      | 2019-12-30 | 17           |
+-- +-------------+--------+------------+--------------+
+-- Output: 
+-- +--------+------------+-------+
+-- | gender | day        | total |
+-- +--------+------------+-------+
+-- | F      | 2019-12-30 | 17    |
+-- | F      | 2019-12-31 | 40    |
+-- | F      | 2020-01-01 | 57    |
+-- | F      | 2020-01-07 | 80    |
+-- | M      | 2019-12-18 | 2     |
+-- | M      | 2019-12-25 | 13    |
+-- | M      | 2019-12-30 | 26    |
+-- | M      | 2019-12-31 | 29    |
+-- | M      | 2020-01-07 | 36    |
+-- +--------+------------+-------+
+-- Explanation: 
+-- For the female team:
+-- The first day is 2019-12-30, Priyanka scored 17 points and the total score for the team is 17.
+-- The second day is 2019-12-31, Priya scored 23 points and the total score for the team is 40.
+-- The third day is 2020-01-01, Aron scored 17 points and the total score for the team is 57.
+-- The fourth day is 2020-01-07, Alice scored 23 points and the total score for the team is 80.
+
+-- For the male team:
+-- The first day is 2019-12-18, Jose scored 2 points and the total score for the team is 2.
+-- The second day is 2019-12-25, Khali scored 11 points and the total score for the team is 13.
+-- The third day is 2019-12-30, Slaman scored 13 points and the total score for the team is 26.
+-- The fourth day is 2019-12-31, Joe scored 3 points and the total score for the team is 29.
+-- The fifth day is 2020-01-07, Bajrang scored 7 points and the total score for the team is 36.
+
+DROP TABLE scores;
+CREATE TABLE Scores (
+    player_name VARCHAR(50),
+    gender VARCHAR(1),
+    day DATE,
+    score_points INT,
+    PRIMARY KEY (gender, day, player_name)
+);
+INSERT INTO Scores (player_name, gender, day, score_points)
+VALUES 
+('Aron', 'F', '2020-01-01', 17),
+('Alice', 'F', '2020-01-07', 23),
+('Bajrang', 'M', '2020-01-07', 7),
+('Khali', 'M', '2019-12-25', 11),
+('Slaman', 'M', '2019-12-30', 13),
+('Joe', 'M', '2019-12-31', 3),
+('Jose', 'M', '2019-12-18', 2),
+('Priya', 'F', '2019-12-31', 23),
+('Priyanka', 'F', '2019-12-30', 17);
+
+-- method 1
+SELECT gender,day,SUM(score_points) OVER(ORDER BY day) as total
+FROM Scores
+WHERE gender='F'
+UNION 
+SELECT gender,day,SUM(score_points) OVER(ORDER BY day)
+FROM Scores
+WHERE gender='M'
+ORDER BY gender,day;
+
+--method 2
+SELECT gender,day,SUM(score_points) OVER(PARTITION BY gender ORDER BY gender,day ROWS BETWEEN UNBOUNDED
+PRECEDING AND CURRENT ROW) as total
+FROM Scores;
+
+
+-- 1285. Find the Start and End Number of Continuous Ranges
+-- Description
+-- Table: Logs
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | log_id        | int     |
+-- +---------------+---------+
+-- log_id is the column of unique values for this table.
+-- Each row of this table contains the ID in a log Table.
+-- Write a solution to find the start and end number of continuous ranges in the table Logs.
+-- Return the result table ordered by start_id.
+-- The result format is in the following example.
+-- Example 1:
+-- Input: 
+-- Logs table:
+-- +------------+
+-- | log_id     |
+-- +------------+
+-- | 1          |
+-- | 2          |
+-- | 3          |
+-- | 7          |
+-- | 8          |
+-- | 10         |
+-- +------------+
+-- Output: 
+-- +------------+--------------+
+-- | start_id   | end_id       |
+-- +------------+--------------+
+-- | 1          | 3            |
+-- | 7          | 8            |
+-- | 10         | 10           |
+-- +------------+--------------+
+-- Explanation: 
+-- The result table should contain all ranges in table Logs.
+-- From 1 to 3 is contained in the table.
+-- From 4 to 6 is missing in the table
+-- From 7 to 8 is contained in the table.
+-- Number 9 is missing from the table.
+-- Number 10 is contained in the table.
+
+DROP TABLE Logs;
+CREATE TABLE Logs (
+    log_id INT PRIMARY KEY
+);
+INSERT INTO Logs (log_id)
+VALUES 
+(1),
+(2),
+(3),
+(7),
+(8),
+(10);
+
+-- m1
+with cte as(
+    SELECT log_id, log_id - ROW_NUMBER() OVER(order by log_id) as rnk
+    FROM Logs
+)
+SELECT Min(log_id) as start_id, Max(log_id) as end_id
+FROM cte
+GROUP BY rnk
+ORDER BY start_id;
+
+
+-- m2 claude over engineered solution
+SELECT 
+    MIN(a.log_id) AS start_id,
+    MAX(a.log_id) AS end_id
+FROM Logs a
+LEFT JOIN Logs b ON a.log_id = b.log_id + 1
+LEFT JOIN Logs c ON a.log_id = c.log_id - 1
+WHERE b.log_id IS NULL  -- Start of a range (no previous consecutive)
+   OR c.log_id IS NULL  -- End of a range (no next consecutive)
+   OR (b.log_id IS NOT NULL AND c.log_id IS NOT NULL)
+GROUP BY a.log_id - 
+    (SELECT COUNT(*) FROM Logs WHERE log_id <= a.log_id)
+ORDER BY start_id;
+
+
+-- m3 
+
+SELECT 
+    MIN(log_id) AS start_id,
+    MAX(log_id) AS end_id
+FROM Logs
+GROUP BY log_id - (SELECT COUNT(*) FROM Logs l2 WHERE l2.log_id <= Logs.log_id)
+ORDER BY start_id;
+
