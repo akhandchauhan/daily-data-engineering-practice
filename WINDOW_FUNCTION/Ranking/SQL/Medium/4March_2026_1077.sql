0e93bbc8c9c452399d78dc6c70d07e79b71bfefc
akhandchauhan <coderasdhero@gmail.com>
2026-03-04 18:28:42 +0530

    modified some text

--- WINDOW_FUNCTION/Ranking/SQL/Medium/4March_2026_1077.sql
+++ WINDOW_FUNCTION/Ranking/SQL/Medium/4March_2026_1077.sql
@@ -128,3 +128,22 @@ JOIN Employee e
 JOIN max_exp m
     ON p.project_id = m.project_id
    AND e.experience_years = m.max_exp;
+
+----------------------------------------------------------------------------------------------------------
+
+--m3 
+WITH cte AS (
+    SELECT 
+        p.project_id,
+        p.employee_id,
+        e.experience_years,
+        MAX(e.experience_years) OVER (PARTITION BY p.project_id) AS max_exp
+    FROM Project p
+    LEFT JOIN Employee e
+        ON p.employee_id = e.employee_id
+)
+SELECT 
+    project_id,
+    employee_id
+FROM cte
+WHERE experience_years = max_exp;
