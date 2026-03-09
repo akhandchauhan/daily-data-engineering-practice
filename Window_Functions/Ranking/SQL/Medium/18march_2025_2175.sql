7f6c88a54a48ead9918d7f5fa08341b17ae4d872
akhandchauhan <coderasdhero@gmail.com>
2026-03-09 14:27:51 +0530

    solved leetcode 2175 sql  solution on 9March 2026

--- Window_Functions/Ranking/SQL/Medium/18march_2025_2175.sql
+++ Window_Functions/Ranking/SQL/Medium/18march_2025_2175.sql
@@ -1,68 +1,3 @@
--- 1126. Active Businesses
--- Table: Events
--- +---------------+---------+
--- | Column Name   | Type    |
--- +---------------+---------+
--- | business_id   | int     |
--- | event_type    | varchar |
--- | occurences    | int     |
--- +---------------+---------+
--- (business_id, event_type) is the primary key of this table.
--- Each row in the table logs the info that an event of some type occured at some business for
--- a number of times.
--- Write an  SQL query to find all active businesses.
--- An active business is a business that has more than one event type with occurences greater 
---than the average occurences of that event type among all businesses.
--- Events table:
--- +-------------+------------+------------+
--- | business_id | event_type | occurences |
--- +-------------+------------+------------+
--- | 1           | reviews    | 7          |
--- | 3           | reviews    | 3          |
--- | 1           | ads        | 11         |
--- | 2           | ads        | 7          |
--- | 3           | ads        | 6          |
--- | 1           | page views | 3          |
--- | 2           | page views | 12         |
--- +-------------+------------+------------+
--- Result table:
--- +-------------+
--- | business_id |
--- +-------------+
--- | 1           |
--- +-------------+
--- Average for 'reviews', 'ads' and 'page views' are (7+3)/2=5, (11+7+6)/3=8, (3+12)/2=7.5 respectively.
--- Business with id 1 has 7 'reviews' events (more than 5) and 11 'ads' events (more than 8) so it is an 
---active business.
-
-CREATE TABLE Events (
-    business_id INT,
-    event_type VARCHAR(255),
-    occurences INT,
-    PRIMARY KEY (business_id, event_type)
-);
-
-INSERT INTO Events (business_id, event_type, occurences) VALUES
-(1, 'reviews', 7),
-(3, 'reviews', 3),
-(1, 'ads', 11),
-(2, 'ads', 7),
-(3, 'ads', 6),
-(1, 'page views', 3),
-(2, 'page views', 12);
-
-
-WITH cte as(
-SELECT *, AVG(occurences) OVER(PARTITION BY event_type) as avg_occ
-FROM Events
-)
-SELECT business_id
-FROM cte
-WHERE occurences > avg_occ
-GROUP BY business_id
-HAVING COUNT(DISTINCT event_type) > 1;
-
-
 -- 2175. The Change in Global Rankings 
 -- Description
 -- Table: TeamPoints
@@ -182,6 +117,7 @@ VALUES
 (4, 13),
 (1, -22);
 
+-- m1 
 WITH cte as(
 SELECT *, ROW_NUMBER() OVER(ORDER BY points DESC,name) as prev_rnk
 FROM TeamPoints
@@ -191,3 +127,17 @@ CAST(prev_rnk AS signed) - CAST(ROW_NUMBER() OVER(ORDER BY points+points_change 
 FROM cte c
 LEFT JOIN PointsChange pc
 ON c.team_id = pc.team_id
+
+
+------------------------------------------------------------------------------------------------------------------
+
+--m2
+
+SELECT 
+        t.team_id,
+        t.name,
+        CAST(ROW_NUMBER() OVER(ORDER BY t.points DESC, t.name) AS signed) -
+        CAST(ROW_NUMBER() OVER(ORDER BY t.points+p.points_change DESC, t.name) AS SIGNED)AS rank_diff
+FROM teampoints t 
+JOIN PointsChange p 
+ON t.team_id = p.team_id;
