2192e591da25917d06edc4e235f8614b013ed688
akhandchauhan <coderasdhero@gmail.com>
2026-03-18 10:15:17 +0530

    solved leetcode 1107 sql question on 18March, 2026

--- Window_Functions/Ranking/SQL/Medium/20dec_2024_1613.sql
+++ Window_Functions/Ranking/SQL/Medium/20dec_2024_1613.sql
@@ -10,7 +10,7 @@
 -- customer_id is the column with unique values for this table.
 -- Each row of this table contains the name and the id customer.
 -- Write a solution to find the missing customer IDs. The missing IDs are ones that are not in the Customers table but 
---are in the range between 1 and the maximum customer_id present in the table.
+-- are in the range between 1 and the maximum customer_id present in the table.
 -- Notice that the maximum customer_id will not exceed 100.
 -- Return the result table ordered by ids in ascending order.
 -- The result format is in the following example.
