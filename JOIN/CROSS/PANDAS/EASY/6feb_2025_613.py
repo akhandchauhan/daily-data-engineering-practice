1f04ce636a6b56041527b88aa9cb05027a6ad9eb
akhandchauhan <coderasdhero@gmail.com>
2026-04-01 10:14:55 +0530

    solved leetcode 613  sql question on 1 April 2026

---
+++ JOIN/CROSS/PANDAS/EASY/6feb_2025_613.py
@@ -0,0 +1,36 @@
+# 613. Shortest Distance in a Line
+# Table point holds the x coordinate of some points on x-axis in a plane, which are all integers.
+# Write a query to find the shortest distance between two points in these points.
+# | x   |
+# |-----|
+# | -1  |
+# | 0   |
+# | 2   |
+# The shortest distance is '1' obviously, which is from point '-1' to '0'. So the output is as below:
+# | shortest|
+# |---------|
+# | 1       |
+# Note: Every point is unique, which means there is no duplicates in table point.
+
+import pandas as pd
+
+# Creating the DataFrame
+data = {'x': [-1, 0, 2]}  # Given points
+point = pd.DataFrame(data)
+
+def shortest_distance(point: pd.DataFrame) -> pd.DataFrame:
+    merged_df = pd.merge(point, point, how='cross')
+
+    # Compute absolute differences, ignoring same points (where x_x == x_y)
+    merged_df = merged_df[merged_df['x_x'] != merged_df['x_y']]
+    merged_df['distance'] = abs(merged_df['x_x'] - merged_df['x_y'])
+
+    # Get the shortest distance
+    min_distance = merged_df['distance'].min()
+
+    return pd.DataFrame({'shortest': [min_distance]})
+    
+    
+
+
+print(shortest_distance(point))
---
+++ JOIN/CROSS/SQL/EASY/6sept_2024_613.sql
@@ -0,0 +1,70 @@
+-- 613. Shortest Distance in a Line
+-- Table point holds the x coordinate of some points on x-axis in a plane, 
+--which are all integers.
+-- Write a query to find the shortest distance between two points in these points.
+-- | x   |
+-- |-----|
+-- | -1  |
+-- | 0   |
+-- | 2   |
+-- The shortest distance is '1' obviously, which is from point '-1' to '0'. So
+-- the output is as below:
+-- | shortest|
+-- |---------|
+-- | 1       |
+-- Note: Every point is unique, which means there is no duplicates in table point.
+-- Follow-up: What if all these points have an id and are arranged from the left most
+-- to the right 
+--most of x axis?
+DROP TABLE Points;
+CREATE TABLE points (
+    x  INT
+);
+
+INSERT INTO points (x ) VALUES (-1),(0),(2);
+
+
+SELECT  MIN(ABS(p1.x - p2.x)) AS shortest
+FROM points p1 
+CROSS JOIN points p2
+WHERE p1.x <> p2.x;
+
+---------------------------------------------------------------------------------------
+-- m2 
+select min(abs(p2.x-p1.x)) as shortest 
+from Point p1 
+join Point p2
+where p1.x<p2.x;
+
+---------------------------------------------------------------------------------------
+-- m3
+SELECT MIN(x - prev_x) AS shortest
+FROM (
+    SELECT 
+        x,
+        LAG(x) OVER (ORDER BY x) AS prev_x
+    FROM points
+) t
+WHERE prev_x IS NOT NULL;
+
+---------------------------------------------------------------------------------------
+-- Follow-up question:
+
+CREATE TABLE points2 (id INT,x INT);
+
+INSERT INTO points2 VALUES (1,-1),(2,0),(3,2),(4,2),(5,2);
+
+-- m1
+SELECT MIN(ABS(p2.x-p1.x)) AS shortest
+FROM points2 p1
+JOIN points2 p2
+ON p1.id < p2.id ;
+
+-- m2 
+SELECT MIN(x - prev_x) AS shortest
+FROM (
+    SELECT x, LAG(x) OVER (ORDER BY x) AS prev_x
+    FROM points2
+) t
+WHERE prev_x IS NOT NULL;
+
