71378c7aed63d8c73e5753e4b7781a7899c6eb93
akhandchauhan <coderasdhero@gmail.com>
2026-05-02 17:31:36 +0530

    solved leetcode 1445 pandas on 2 May, 2026

--- CASE WHEN/PANDAS/MEDIUM/1march_2025_1445.py
+++ CASE WHEN/PANDAS/MEDIUM/1march_2025_1445.py
@@ -10,6 +10,7 @@
 # -- +---------------+---------+
 # -- (sale_date, fruit) is the primary key (combination of columns with unique values) of this table.
 # -- This table contains the sales of "apples" and "oranges" sold each day.
+
 # -- Write a solution to report the difference between the number of  apples and  oranges sold each day.
 # -- Return the result table ordered by sale_date.
 # -- Inpt: 
@@ -40,7 +41,9 @@
 # -- Day 2020-05-02, 15 apples and 15 oranges were sold (Difference 15 - 15 = 0).
 # -- Day 2020-05-03, 20 apples and 0 oranges were sold (Difference 20 - 0 = 20).
 # -- Day 2020-05-04, 15 apples and 16 oranges were sold (Difference 15 - 16 = -1).
+
 import pandas as pd
+import numpy as np
 
 data = {
     'sale_date': ['2020-05-01', '2020-05-01', '2020-05-02', '2020-05-02', '2020-05-03', '2020-05-03', '2020-05-04', '2020-05-04'],
@@ -51,4 +54,39 @@ df = pd.DataFrame(data)
 
 df_pivot = df.pivot(index = 'sale_date', columns = 'fruit',values = 'sold_num').reset_index()
 df_pivot['diff'] = df_pivot['apples'] - df_pivot['oranges']
-print(df_pivot[['sale_date','diff']])
+#print(df_pivot[['sale_date','diff']])
+
+
+##################################################################################################
+# m2 
+
+final_df = (
+    df.assign(
+        sold_quantity = lambda d: np.where(d['fruit'] == 'apples',d['sold_num'], -d['sold_num'])
+    )
+    .groupby('sale_date', as_index = False)['sold_quantity']
+    .sum()
+    .rename(columns = {'sold_quantity':'diff'})
+    .sort_values('sale_date')
+)
+# print(final_df)
+
+##################################################################################################
+# m3
+
+final_df = df.copy()
+
+final_df = (
+    final_df.sort_values(['sale_date','fruit'])
+    .assign(
+        next_quantity = lambda d: d.groupby('sale_date')['sold_num'].shift(-1)
+    )
+    .loc[lambda d: d['fruit']  == 'apples']
+    .assign(
+        diff = lambda d: d['sold_num'] - d['next_quantity']
+    )
+    .sort_values('sale_date')
+    [['sale_date','diff']]
+)
+
+print(final_df)
