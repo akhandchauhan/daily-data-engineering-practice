4e6edafff6ea35e97069d67504501b096003b316
akhandchauhan <coderasdhero@gmail.com>
2026-05-26 08:12:27 +0530

    solved leetcode 1098 sql  on 26 May, 2026

--- JOIN/LEFT/SQL/MEDIUM/22oct_2024_1098.sql
+++ JOIN/LEFT/SQL/MEDIUM/22oct_2024_1098.sql
@@ -1,4 +1,5 @@
 -- 1098. Unpopular Books
+
 -- Table: Books
 -- +----------------+---------+
 -- | Column Name    | Type    |
@@ -8,6 +9,7 @@
 -- | available_from | date    |
 -- +----------------+---------+
 -- book_id is the primary key of this table.
+
 -- Table: Orders
 -- +----------------+---------+
 -- | Column Name    | Type    |
@@ -19,10 +21,12 @@
 -- +----------------+---------+
 -- order_id is the primary key of this table.
 -- book_id is a foreign key to the Books table.
--- Write an SQL query that reports the books that have sold less than 10 copies in the last year,
+
+-- Write an SQL query that reports the books that have sold less than 10 
+-- copies in the last year,
 -- excluding books that have been available for less than 1 month from today.
 -- Assume today is 2019-06-23.
--- The query result format is in the following example:
+
 -- Books table:
 -- +---------+--------------------+----------------+
 -- | book_id | name               | available_from |
@@ -59,20 +63,26 @@ DROP TABLE IF EXISTS Orders;
 Create table If Not Exists Books (book_id int, name varchar(50), available_from date);
 Create table If Not Exists Orders (order_id int, book_id int, quantity int, dispatch_date date);
 Truncate table Books;
-insert into Books (book_id, name, available_from) values ('1', 'Kalila And Demna', '2010-01-01');
-insert into Books (book_id, name, available_from) values ('2', '28 Letters', '2012-05-12');
-insert into Books (book_id, name, available_from) values ('3', 'The Hobbit', '2019-06-10');
-insert into Books (book_id, name, available_from) values ('4', '13 Reasons Why', '2019-06-01');
-insert into Books (book_id, name, available_from) values ('5', 'The Hunger Games', '2008-09-21');
-Truncate table Orders;
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('1', '1', '2', '2018-07-26');
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('2', '1', '1', '2018-11-05');
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('3', '3', '8', '2019-06-11');
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('4', '4', '6', '2019-06-05');
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('5', '4', '5', '2019-06-20');
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('6', '5', '9', '2009-02-02');
-insert into Orders (order_id, book_id, quantity, dispatch_date) values ('7', '5', '8', '2010-04-13');
+INSERT INTO Books (book_id, name, available_from)
+VALUES
+(1, 'Kalila And Demna', '2010-01-01'),
+(2, '28 Letters', '2012-05-12'),
+(3, 'The Hobbit', '2019-06-10'),
+(4, '13 Reasons Why', '2019-06-01'),
+(5, 'The Hunger Games', '2008-09-21');
 
+TRUNCATE TABLE Orders;
+INSERT INTO Orders (order_id, book_id, quantity, dispatch_date)
+VALUES
+(1, 1, 2, '2018-07-26'),
+(2, 1, 1, '2018-11-05'),
+(3, 3, 8, '2019-06-11'),
+(4, 4, 6, '2019-06-05'),
+(5, 4, 5, '2019-06-20'),
+(6, 5, 9, '2009-02-02'),
+(7, 5, 8, '2010-04-13');
+
+-- m1
 SELECT book_id, name
 FROM Books
 LEFT JOIN Orders
@@ -80,3 +90,19 @@ USING (book_id)
 WHERE available_from < '2019-05-23'
 GROUP BY 1, 2
 HAVING SUM(IF(dispatch_date >= '2018-06-23', quantity, 0)) < 10;
+
+--------------------------------------------------------------------------------
+-- m2
+SELECT 
+    b.book_id,
+    b.name
+FROM Books b 
+LEFT JOIN Orders o 
+ON b.book_id = o.book_id
+AND o.dispatch_date BETWEEN DATE_SUB('2019-06-23', INTERVAL 1 YEAR)
+                        AND '2019-06-23'
+WHERE DATEDIFF('2019-06-23',b.available_from) > 30
+GROUP BY 
+        b.book_id,
+        b.name
+HAVING COALESCE(SUM(o.quantity), 0) < 10;
