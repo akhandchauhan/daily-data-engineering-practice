8fdbd92bfe6be8670373479a0bcf5c9ebfcea539
akhandchauhan <coderasdhero@gmail.com>
2026-06-13 11:16:37 +0530

    solved ankitbansal 023 sql on 13 June, 2026

---
+++ Ankit_13_June_2026_023_Covid_Cases.sql
@@ -0,0 +1,14 @@
+-- find students with same marks in physics and chemistry
+
+create table covid(city varchar(50),days date,cases int);
+delete from covid;
+
+INSERT INTO covid VALUES
+('DELHI','2022-01-01',100),('DELHI','2022-01-02',200),('DELHI','2022-01-03',300),
+('MUMBAI','2022-01-01',100),('MUMBAI','2022-01-02',100),('MUMBAI','2022-01-03',300),
+('CHENNAI','2022-01-01',100),('CHENNAI','2022-01-02',200),('CHENNAI','2022-01-03',150),
+('BANGALORE','2022-01-01',100),('BANGALORE','2022-01-02',300),('BANGALORE','2022-01-03',200),
+('BANGALORE','2022-01-04',400);
+
+
+
